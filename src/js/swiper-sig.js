24825f7bd15294f968c562a031961e4e193707f8
Anastasiia Bulba <fedinanastya@gmail.com>
2025-05-13 19:39:57 +0300

    swaper mobil fix

--- src/js/swiper-sig.js
+++ src/js/swiper-sig.js
@@ -1,15 +1,23 @@
 import Swiper from 'swiper';
 import 'swiper/css';
 
+// const swiperSignature = new Swiper('.nedenSwiper', {
+//   loop: true,
+//   slidesPerView: 0.5,
+//   spaceBetween: 4,
+//   allowTouchMove: true,
+//   breakpoints: {
+//     1440: {
+//       slidesPerView: 2.5,
+//       spaceBetween: 4,
+//     },
+//   },
+// });
+
 const swiperSignature = new Swiper('.nedenSwiper', {
-  loop: true,
-  slidesPerView: 0.5,
-  spaceBetween: 4,
+  loop: false,
+  slidesPerView: 'auto',
+  spaceBetween: 8,
   allowTouchMove: true,
-  breakpoints: {
-    1440: {
-      slidesPerView: 2.5,
-      spaceBetween: 4,
-    },
-  },
+  freeMode: true,
 });
